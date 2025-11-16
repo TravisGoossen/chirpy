6455cabd8b525a1cb835a9be4dabaa93c7b19216
TravisGoossen <tgoossen@outlook.com>
2025-11-15 22:52:28 -0700

    Created basic index.html /app route. Added /healthz readiness endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TravisGoossen/chirpy
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", ReadinessEndpoint)
+	server.ListenAndServe()
+
+}
+
+func ReadinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
