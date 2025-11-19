f3f49b7b6022aa9661a655d1dca0df8bcdeb897b
TravisGoossen <tgoossen@outlook.com>
2025-11-18 19:56:39 -0700

    Added middlewareMetricsInc, the /metrics route, and the /reset route

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) displayMetrics(w http.ResponseWriter, r *http.Request) {
+	currentHits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", currentHits)))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", ReadinessEndpoint)
+	mux.HandleFunc("/metrics", apiCfg.displayMetrics)
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
 	server.ListenAndServe()
 
 }
