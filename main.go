6b10adacd492c3a47bbe714db36f53d4fc703269
TravisGoossen <tgoossen@outlook.com>
2025-11-21 13:18:23 -0700

    Added validateChirpEndpoint handler + route, and helper function writeJSONResponse

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -19,9 +20,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) displayMetrics(w http.ResponseWriter, r *http.Request) {
 	currentHits := cfg.fileserverHits.Load()
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", currentHits)))
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+	`, currentHits)))
 }
 
 func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
@@ -36,15 +44,56 @@ func main() {
 		Handler: mux,
 	}
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/healthz", ReadinessEndpoint)
-	mux.HandleFunc("/metrics", apiCfg.displayMetrics)
-	mux.HandleFunc("/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("GET /api/healthz", ReadinessEndpoint)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.displayMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpEndpoint)
 	server.ListenAndServe()
+}
 
+func writeJSONResponse(w http.ResponseWriter, statusCode int, data any) {
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, `{"error":"Something went wrong"}`, 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(dataJSON)
 }
 
 func ReadinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func validateChirpEndpoint(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type responseBody struct {
+		Valid bool   `json:"valid,omitempty"`
+		Error string `json:"error,omitempty"`
+	}
+
+	reqBody := requestBody{}
+	respBody := responseBody{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&reqBody)
+	if err != nil {
+		respBody.Error = "Something went wrong"
+		writeJSONResponse(w, 500, respBody)
+		return
+	}
+
+	if len(reqBody.Body) > 140 {
+		respBody.Error = "Chirp is too long"
+		writeJSONResponse(w, 400, respBody)
+		return
+	}
+
+	respBody.Valid = true
+	writeJSONResponse(w, 200, respBody)
 }
